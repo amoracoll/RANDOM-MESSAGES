d9a0967f60d24e31bbf0df34105e20f99c33e8cf
DESKTOP-38JM5KT\arnau.mora_modelical <arnau.mora@modelical.com>
2023-11-16 22:06:45 +0100

    Modificacion general

---
+++ macro.js
@@ -0,0 +1,19 @@
+// Lista de frases aleatorias
+var frasesAleatorias = [
+    "La vida es como una bicicleta, para mantener el equilibrio, debes seguir adelante.",
+    "La creatividad es inteligencia divirtiéndose.",
+    "El éxito es ir de fracaso en fracaso sin perder entusiasmo.",
+    "La única manera de hacer un gran trabajo es amar lo que haces.",
+    "La paciencia es amarga, pero su fruto es dulce.",
+    "No te preocupes por los fracasos, preocúpate por las posibilidades que pierdes cuando ni siquiera lo intentas.",
+    "El único modo de hacer un gran trabajo es amar lo que haces.",
+    "La vida es 10% lo que me pasa y 90% cómo reacciono a ello.",
+    "Si buscas resultados distintos, no hagas siempre lo mismo.",
+    "El secreto del éxito es aprender a usar el dolor y el placer en lugar de dejar que te use a ti.",
+];
+
+// Obtener una frase aleatoria
+function obtenerFraseAleatoria() {
+    var indiceAleatorio = Math.floor(Math.random() * frasesAleatorias.length);
+    return frasesAleatorias[indiceAleatorio];
+}
